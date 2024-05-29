2ef5bb42ca94c8df78bf420ec3369aa7252742ff
Yevhen-Pashkulskyi <pashkulskyevhen@gmail.com>
2024-05-29 15:01:28 +0300

    Initial project version

---
+++ src/main/java/com/example/App/SpringSecurityContactApplication.java
@@ -0,0 +1,13 @@
+package com.example.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityContactApplication.class, args);
+	}
+
+}
